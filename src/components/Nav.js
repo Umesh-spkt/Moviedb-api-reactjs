03d3697f24e47c8d1327a383bcd8825e8e2f20fd
Umesh Sapkota <sapkotaumesh4@gmial.com>
2021-02-08 16:00:21 +0545

    first commint

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <div class="nav-wrapper container">
+                <a href="#" class="brand-logo">Movie finder</a>                
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
