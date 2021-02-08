db8eb3160ab483a5441ce5e7bd491bf3fb30e49c
Umesh Sapkota <sapkotaumesh4@gmial.com>
2021-02-08 16:17:37 +0545

    Updates

---
+++ precache-manifest.f4006eb8c1512bfc1e2e2f5b15cf4edc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f7c2f0e07e7bda94657",
+    "url": "/Moviedb-api-reactjs/static/js/main.3f7c2f0e.chunk.js"
+  },
+  {
+    "revision": "c20e3088087bc71ac6c6",
+    "url": "/Moviedb-api-reactjs/static/js/1.c20e3088.chunk.js"
+  },
+  {
+    "revision": "d69a6523102b1c8e612e",
+    "url": "/Moviedb-api-reactjs/static/js/runtime~main.d69a6523.js"
+  },
+  {
+    "revision": "ce7e7bf7c2ea4d1e3dd1dd0107b2b78b",
+    "url": "/Moviedb-api-reactjs/index.html"
+  }
+];
